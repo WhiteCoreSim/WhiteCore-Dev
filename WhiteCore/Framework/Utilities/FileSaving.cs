3017ab635137c4c5cb7935812397fe27370eb198
Rowan Deppeler <greythane@gmail.com>
2014-01-07 08:13:16 +1100

    Update of file copyright info
    Add back credit for http://aurora-sim.org inadvertently removed in
    refactoring

--- WhiteCore/Framework/Utilities/FileSaving.cs
+++ WhiteCore/Framework/Utilities/FileSaving.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (c) Contributors, http://WhiteCore-sim.org/
+ * Copyright (c) Contributors, http://whitecore-sim.org/, http://aurora-sim.org
  * See CONTRIBUTORS.TXT for a full list of copyright holders.
  *
  * Redistribution and use in source and binary forms, with or without
