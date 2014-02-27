1d8795089a52e190fb51085cd45379051724b78d
Rowan D <greythane@gmail.com>
2014-02-28 10:12:31 +1100

    Update 'Island' default heights
    * TerrainChannel
        - updated default height setting for Island creation. (was tending
    to create submerged islands)
    
    * setupversioninfo.bat (bin) - revamped to use std git commit hashcode

--- bin/setupversioninfo.bat
+++ bin/setupversioninfo.bat
@@ -6,8 +6,9 @@ for /f "tokens=* delims= " %%a in (..\.git\logs\HEAD) do (
 set var=%%a
 )
 for /f "tokens=1-7" %%a in ("%var%") do (
-echo WhiteCore-%%b %%f>.version
+  set commit= %%b
+  set chars=!commit:~1,7!
+  echo %DATE%-!chars!^ 1> .version
 )
 
 :DIE
-pause
