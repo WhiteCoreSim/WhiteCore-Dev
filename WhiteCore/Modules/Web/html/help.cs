ef339732452e9153a007b73261c5a59a796badde
Fly Man <fly.man.opensim@gmail.com>
2014-05-04 02:09:14 +0200

    WebUI Handler
    * Updated the Help.cs

--- WhiteCore/Modules/Web/html/help.cs
+++ WhiteCore/Modules/Web/html/help.cs
@@ -36,11 +36,12 @@ namespace WhiteCore.Modules.Web
             vars.Add("HelpText", translator.GetTranslatedString("HelpText"));
             vars.Add("HelpViewersConfigText", translator.GetTranslatedString("HelpViewersConfigText"));
             vars.Add("AngstormViewer", translator.GetTranslatedString("AngstormViewer"));
-            vars.Add("VoodooViewer", translator.GetTranslatedString("VoodooViewer"));
             vars.Add("AstraViewer", translator.GetTranslatedString("AstraViewer"));
+            vars.Add("FirestormViewer", translator.GetTranslatedString("FirestormViewer"));
             vars.Add("ImprudenceViewer", translator.GetTranslatedString("ImprudenceViewer"));
             vars.Add("PhoenixViewer", translator.GetTranslatedString("PhoenixViewer"));
             vars.Add("SingularityViewer", translator.GetTranslatedString("SingularityViewer"));
+            vars.Add("VoodooViewer", translator.GetTranslatedString("VoodooViewer"));
             vars.Add("ZenViewer", translator.GetTranslatedString("ZenViewer"));
 
             return vars;
