3731204874fb885bd658c97709d6383439df8e30
Rowan Deppeler <greythane@gmail.com>
2014-08-19 10:50:33 +1000

    Default MySql table creation to UTF8
    New MySql table creation will default to UTF8 charset & encoding.
    (requirement for unicode DisplayNames)

--- WhiteCore/DataManagerPlugins/MySQL/MySQLDataManager.cs
+++ WhiteCore/DataManagerPlugins/MySQL/MySQLDataManager.cs
@@ -643,7 +643,7 @@ namespace WhiteCore.DataManager.MySQL
 
             }
 
-            string query = string.Format("create table " + table + " ( {0} {1}) ",
+            string query = string.Format("create table " + table + " ( {0} {1}) DEFAULT CHARSET=utf8 COLLATE=utf8_bin",
                                          string.Join(", ", columnDefinition.ToArray()),
                                          indicesQuery.Count > 0
                                              ? ", " + string.Join(", ", indicesQuery.ToArray())
