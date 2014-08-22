55e578e483d7e7df88f168b72235f133b89f54ab
Rowan D <greythane@gmail.com>
2014-08-22 17:11:54 +1000

    Correct Windows startup scripts
    Correct Windows startup batch files so that they start from the
    directory they are executed from.

--- WhiteCoreSim/grid_console.bat
+++ WhiteCoreSim/grid_console.bat
@@ -4,10 +4,13 @@
 ECHO =======================================
 ECHO Starting WhiteCore Grid servers . . .
 ECHO =======================================
-cd .\bin
-.\WhiteCore.server.exe -skipconfig
+
+chdir /D  %~dp0
+cd bin
+WhiteCore.Server.exe -skipconfig
 cd ..
 Echo.
 Echo WhiteCore grid servers stopped . . .
 
+set /p nothing= Enter to continue
 exit
--- WhiteCoreSim/sim_console.bat
+++ WhiteCoreSim/sim_console.bat
@@ -3,10 +3,13 @@
 ECHO =======================================
 ECHO Starting WhiteCore Standalone Sim . . .
 ECHO =======================================
+
+chdir /D  %~dp0
 cd .\bin
 .\WhiteCore.exe -skipconfig
 cd ..
 Echo.
 Echo WhiteCore stopped . . .
 
+set /p nothing= Enter to continue
 exit
