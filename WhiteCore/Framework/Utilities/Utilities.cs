f4bf074c83276497790654e2ffb337c73927fc2b
Fly Man <fly.man.opensim@gmail.com>
2014-12-10 16:48:03 +0100

    Currency
    * Added a new function GetNextWeekDay which returns a DateTime in preparation for the Scheduler changes

--- WhiteCore/Framework/Utilities/Utilities.cs
+++ WhiteCore/Framework/Utilities/Utilities.cs
@@ -578,6 +578,13 @@ namespace WhiteCore.Framework.Utilities
                 return Environment.Is64BitOperatingSystem;
             }
         }
+        
+        public static DateTime GetNextWeekday(DateTime start, DayOfWeek day)
+        {
+        	// The (... + 7) % 7 ensures we end up with a value in the range [0, 6]
+        	int daysToAdd = ((int) day - (int) start.DayOfWeek + 7) % 7;
+        	return start.AddDays(daysToAdd);
+        }
 
         public static class RandomPassword
         {
