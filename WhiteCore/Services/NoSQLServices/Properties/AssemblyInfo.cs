8d7847e00c6bdf4be1d1a54c136fb9e1b4dc8ee6
Fly Man <fly.man.opensim@gmail.com>
2016-01-03 23:31:41 +0100

    Copyright updates
    * Changing all Copyrights to the new year, minor changes to older versions

--- WhiteCore/Services/NoSQLServices/Properties/AssemblyInfo.cs
+++ WhiteCore/Services/NoSQLServices/Properties/AssemblyInfo.cs
@@ -37,7 +37,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WhiteCore.Services.NoSQLServices")]
-[assembly: AssemblyCopyright("Copyright © 2014-2015")]
+[assembly: AssemblyCopyright("Copyright © 2014-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
