df05ce50f941d1b90941514ab927e5c2e811462d
Greythane (Rowan Deppeler) <greythane@gmail.com>
2016-05-01 15:14:07 +1000

    Serialization/Archive* - Correct resource leaks in ArchiveHelper - Code refactoring

--- WhiteCore/Framework/Serialization/SceneEntitySerializer.cs
+++ WhiteCore/Framework/Serialization/SceneEntitySerializer.cs
@@ -27,27 +27,27 @@
 
 using System.IO;
 using System.Xml;
+using OpenMetaverse;
 using WhiteCore.Framework.Modules;
 using WhiteCore.Framework.SceneInfo;
 using WhiteCore.Framework.SceneInfo.Entities;
-using OpenMetaverse;
 
 namespace WhiteCore.Framework.Serialization
 {
     public interface ISceneObjectSerializer
     {
-        ISceneEntity FromOriginalXmlFormat(string serialization, IRegistryCore scene);
+        ISceneEntity FromOriginalXmlFormat (string serialization, IRegistryCore scene);
 
-        ISceneEntity FromOriginalXmlFormat(UUID fromUserInventoryItemID, string xmlData,
+        ISceneEntity FromOriginalXmlFormat (UUID fromUserInventoryItemID, string xmlData,
                                            IRegistryCore scene);
 
-        string ToOriginalXmlFormat(ISceneEntity sceneObject);
-        ISceneEntity FromXml2Format(string xmlData, IScene scene);
-        ISceneEntity FromXml2Format(ref MemoryStream ms, IScene scene);
-        string ToXml2Format(ISceneEntity sceneObject);
-        byte[] ToBinaryXml2Format(ISceneEntity sceneObject);
-        void ToXmlFormat(ISceneChildEntity part, XmlTextWriter writer);
-        void AddSerializer(string p, ISOPSerializerModule serializer);
+        string ToOriginalXmlFormat (ISceneEntity sceneObject);
+        ISceneEntity FromXml2Format (string xmlData, IScene scene);
+        ISceneEntity FromXml2Format (ref MemoryStream ms, IScene scene);
+        string ToXml2Format (ISceneEntity sceneObject);
+        byte [] ToBinaryXml2Format (ISceneEntity sceneObject);
+        void ToXmlFormat (ISceneChildEntity part, XmlTextWriter writer);
+        void AddSerializer (string p, ISOPSerializerModule serializer);
     }
 
     public class SceneEntitySerializer
