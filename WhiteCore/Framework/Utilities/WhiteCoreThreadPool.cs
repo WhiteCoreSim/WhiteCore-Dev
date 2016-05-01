dc578c7622ec080cba55fa976ff0547a0b28dadf
Greythane (Rowan Deppeler) <greythane@gmail.com>
2016-05-01 16:42:09 +1000

    WhiteCoreThreadPool - Additional locking for concurrent data access

--- WhiteCore/Framework/Utilities/WhiteCoreThreadPool.cs
+++ WhiteCore/Framework/Utilities/WhiteCoreThreadPool.cs
@@ -44,127 +44,105 @@ namespace WhiteCore.Framework.Utilities
 
     public class WhiteCoreThreadPool
     {
-        readonly int[] Sleeping;
-        readonly Thread[] Threads;
+        readonly int [] Sleeping;
+        readonly Thread [] Threads;
         readonly WhiteCoreThreadPoolStartInfo m_info;
-        readonly ConcurrentQueue<Action> queue = new ConcurrentQueue<Action>();
+        readonly ConcurrentQueue<Action> queue = new ConcurrentQueue<Action> ();
         public long nSleepingthreads;
         public long nthreads;
 
-        public WhiteCoreThreadPool(WhiteCoreThreadPoolStartInfo info)
+        public WhiteCoreThreadPool (WhiteCoreThreadPoolStartInfo info)
         {
             m_info = info;
-            Threads = new Thread[m_info.Threads];
-            Sleeping = new int[m_info.Threads];
+            Threads = new Thread [m_info.Threads];
+            Sleeping = new int [m_info.Threads];
             nthreads = 0;
             nSleepingthreads = 0;
             // lets threads check for work a bit faster in case we have all sleeping and awake interrupt fails
         }
 
-        void ThreadStart(object number)
+        void ThreadStart (object number)
         {
-            Culture.SetCurrentCulture();
+            Culture.SetCurrentCulture ();
             int OurSleepTime = 0;
 
-            int[] numbers = number as int[];
-            int ThreadNumber = numbers[0];
+            int [] numbers = number as int [];
+            int ThreadNumber = numbers [0];
 
-            while (true)
-            {
-                try
-                {
+            while (true) {
+                try {
                     Action item = null;
-                    if (!queue.TryDequeue(out item))
-                    {
+                    if (!queue.TryDequeue (out item)) {
                         OurSleepTime += m_info.SleepIncrementTime;
-                        if (m_info.KillThreadAfterQueueClear || OurSleepTime > m_info.MaxSleepTime)
-                        {
-                            Threads[ThreadNumber] = null;
-                            Interlocked.Decrement(ref nthreads);
-                            break;
+                        if (m_info.KillThreadAfterQueueClear || OurSleepTime > m_info.MaxSleepTime) {
+                            lock (Threads) {
+                                Threads [ThreadNumber] = null;
+                                Interlocked.Decrement (ref nthreads);
+                                break;
+                            }
                         }
 
-                        Interlocked.Exchange(ref Sleeping[ThreadNumber], 1);
-                        Interlocked.Increment(ref nSleepingthreads);
-                        try
-                        {
-                            Thread.Sleep(OurSleepTime);
-                        }
-                        catch (ThreadInterruptedException)
-                        {
+                        Interlocked.Exchange (ref Sleeping [ThreadNumber], 1);
+                        Interlocked.Increment (ref nSleepingthreads);
+                        try {
+                            Thread.Sleep (OurSleepTime);
+                        } catch (ThreadInterruptedException) {
                         }
 
-                        Interlocked.Decrement(ref nSleepingthreads);
-                        Interlocked.Exchange(ref Sleeping[ThreadNumber], 0);
+                        Interlocked.Decrement (ref nSleepingthreads);
+                        Interlocked.Exchange (ref Sleeping [ThreadNumber], 0);
                         continue;
 
                     }
-             
+
                     // workers have no business on pool waiting times
-                        // that would make interrelations very hard to debug
-                        // If a worker wants to delay its re-queue, then he should for now sleep before
-                        // asking to be re-queued.
-                        // in future we should add a trigger time delay as parameter to the queue request.
-                        // so to release the thread sooner, like .net and mono can now do.
-                        // This control loop would then have to look for those delayed requests.
-                        // UBIT
+                    // that would make interrelations very hard to debug
+                    // If a worker wants to delay its re-queue, then he should for now sleep before
+                    // asking to be re-queued.
+                    // in future we should add a trigger time delay as parameter to the queue request.
+                    // so to release the thread sooner, like .net and mono can now do.
+                    // This control loop would then have to look for those delayed requests.
+                    // UBIT
                     OurSleepTime = m_info.InitialSleepTime;
-                    item.Invoke();
-                   
+                    item.Invoke ();
+
+                } catch {
                 }
-                catch
-                {
-                }
-                Thread.Sleep(OurSleepTime);
+                Thread.Sleep (OurSleepTime);
             }
         }
 
-        public void QueueEvent(Action delegat, int Priority)
+        public void QueueEvent (Action delegat, int Priority)
         {
             if (delegat == null)
                 return;
 
-            queue.Enqueue(delegat);
+            queue.Enqueue (delegat);
 
-            if (nthreads == 0 || (nthreads - nSleepingthreads < queue.Count - 1 && nthreads < Threads.Length))
-            {
-                lock (Threads)
-                {
-                    for (int i = 0; i < Threads.Length; i++)
-                    {
-                        if (Threads[i] == null)
-                        {
-                            Thread thread = new Thread(ThreadStart)
-                                                {
-                                                    Priority = m_info.priority,
-                                                    Name =
+            lock (Threads) {
+                if (nthreads == 0 || (nthreads - nSleepingthreads < queue.Count - 1 && nthreads < Threads.Length)) {
+                    for (int i = 0; i < Threads.Length; i++) {
+                        if (Threads [i] == null) {
+                            Thread thread = new Thread (ThreadStart) {
+                                Priority = m_info.priority,
+                                Name =
                                                         (m_info.Name == "" ? "WhiteCoreThreadPool" : m_info.Name) + "#" + i,
-                                                    IsBackground = true
-                                                };
-                            try
-                            {
-                                thread.Start(new[] {i});
-                                Threads[i] = thread;
-                                Sleeping[i] = 0;
+                                IsBackground = true
+                            };
+                            try {
+                                thread.Start (new [] { i });
+                                Threads [i] = thread;
+                                Sleeping [i] = 0;
                                 nthreads++;
-                            }
-                            catch
-                            {
+                            } catch {
                             }
                             return;
                         }
                     }
-                }
-            }
-            else if (nSleepingthreads > 0)
-            {
-                lock (Threads)
-                {
-                    for (int i = 0; i < Threads.Length; i++)
-                    {
-                        if (Sleeping[i] == 1 && Threads[i].ThreadState == ThreadState.WaitSleepJoin)
-                        {
-                            Threads[i].Interrupt(); // if we have a sleeping one awake it
+                } else if (nSleepingthreads > 0) {
+                    for (int i = 0; i < Threads.Length; i++) {
+                        if (Sleeping [i] == 1 && Threads [i].ThreadState == ThreadState.WaitSleepJoin) {
+                            Threads [i].Interrupt (); // if we have a sleeping one awake it
                             return;
                         }
                     }
@@ -172,62 +150,53 @@ namespace WhiteCore.Framework.Utilities
             }
         }
 
-        public void AbortThread(Thread thread)
+        public void AbortThread (Thread thread)
         {
             int i;
-            lock (Threads)
-            {
-                for (i = 0; i < Threads.Length; i++)
-                {
-                    if (Threads[i] == thread)
+            lock (Threads) {
+                for (i = 0; i < Threads.Length; i++) {
+                    if (Threads [i] == thread)
                         break;
                 }
                 if (i == Threads.Length)
                     return;
 
-                Threads[i] = null;
+                Threads [i] = null;
                 nthreads--;
             }
-            try
-            {
-                if(thread != null)
-                    thread.Abort("Shutdown");
-            }
-            catch
-            {
+            try {
+                if (thread != null)
+                    thread.Abort ("Shutdown");
+            } catch {
             }
         }
 
-        public void Restart()
+        public void Restart ()
         {
-            ClearEvents();
-            var threads = new Thread[0];
-            lock (Threads)
-            {
-                threads = new Thread[Threads.Length];
-                Threads.CopyTo(threads, 0);
+            ClearEvents ();
+            var threads = new Thread [0];
+            lock (Threads) {
+                threads = new Thread [Threads.Length];
+                Threads.CopyTo (threads, 0);
             }
-            foreach (Thread t in threads)
-            {
-                AbortThread(t);
+            foreach (Thread t in threads) {
+                AbortThread (t);
             }
         }
 
-        public void ClearEvents()
+        public void ClearEvents ()
         {
             Action itm;
-            while (queue.TryDequeue(out itm))
-            {
+            while (queue.TryDequeue (out itm)) {
             }
         }
 
-        public Thread[] GetThreads()
+        public Thread [] GetThreads ()
         {
-            var threads = new Thread[0];
-            lock (Threads)
-            {
-                threads = new Thread[Threads.Length];
-                Threads.CopyTo(threads, 0);
+            var threads = new Thread [0];
+            lock (Threads) {
+                threads = new Thread [Threads.Length];
+                Threads.CopyTo (threads, 0);
             }
             return threads;
         }
