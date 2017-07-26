8ccb29485790e41ba224874d11aa9c421525fba0
Greythane (Rowan Deppeler) <greythane@gmail.com>
2017-07-26 20:30:24 +1000

    Separate service startup scripts for *nix - Provide for stating services individually

---
+++ WhiteCoreSim/run_gridservice.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Startup script for WhiteCore-Sim grid server service
+# Versions 0.9.2+
+#
+# July 2017
+# greythane @ gmail.com
+#
+
+cd ./bin
+wait
+echo Starting WhiteCore GridServer...
+screen -S Grid -d -m mono WhiteCore.Server.exe -skipconfig
+sleep 3
+screen -list
+echo "To view the Grid server console, use the command : screen -r Grid"
+echo "To detach fron the console use the command : ctrl+a d  ..ctrl+a > command mode,  d > detach.."
+echo
---
+++ WhiteCoreSim/run_simservice.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Startup script for the WhiteCore-Sim Region server service
+# Versions 0.9.2+
+#
+# July 2017
+# greythane @ gmail.com
+#
+
+cd ./bin
+wait
+echo Starting WhiteCore Region Simulator...
+screen -S Sim -d -m mono WhiteCore.exe -skipconfig
+sleep 3
+screen -list
+echo "To view the Sim server console,  use the command : screen -r Sim"
+echo "To detach fron the console use the command : ctrl+a d  ..ctrl+a > command mode,  d > detach.."
+echo
